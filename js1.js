9da6f72468b93743310d750ba2f1595b75ef9fce
tdhiraj312 <tdhiraj312@gmail.com>
2025-01-20 23:11:40 +0530

    Update js1.js

--- js1.js
+++ js1.js
@@ -2,3 +2,21 @@ function fun() {
     var x = document.getElementById("frm1").value;  
      alert("Hi"+" "+x+" you have successfully submitted the details");  
  }
+
+ function fun1(){
+    document.getElementById("intro").innerHTML = "Summary";
+    document.getElementById("intro").style.color = "yellow";
+    document.getElementById("intro").style.fontSize = "50px";
+}
+
+function fun2(){
+    document.getElementById("summary").style.backgroundColor = "red";
+    document.getElementById("summary").style.fontSize = "30px";
+}
+
+function myFunction() {  
+    document.getElementById("demo").style.fontSize = "25px";   
+    document.getElementById("demo").style.color = "brown";  
+    document.getElementById("demo").style.backgroundColor = "lightgreen";   
+    document.getElementById("demo").innerHTML = "Dhiraj Thakur";        
+}
